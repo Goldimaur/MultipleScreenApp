281211c97c3eedb8762b05b9e37aac3e9e70acdb
goldimaurya <goldi.maurya_cs19@gla.ac.in>
2021-07-14 14:02:53 +0530

    final commit

---
+++ app/src/main/java/com/example/multiplescreenapp/ThirdPage.java
@@ -0,0 +1,24 @@
+package com.example.multiplescreenapp;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ThirdPage extends AppCompatActivity {
+    Button backButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third_page);
+        backButton = findViewById(R.id.button4);
+
+        backButton.setOnClickListener(v -> {
+            Intent intent = new Intent(ThirdPage.this, MainActivity.class);
+            startActivity(intent);
+        });
+    }
+}
